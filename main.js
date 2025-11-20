a16a69ee0fbf701b4c1ec36e547ff58d375b70e4
Miguelpr123 <miguelperezrodriguez335@gmail.com>
2025-11-20 13:04:48 -0500

    Recording Buttons

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 let rec;
 
-if(!("webkitSpeechRecognition" in window) ){
-    alert('sin reconocimiento de voz');
-}else{
-    rec = new webkitSpeechRecognition();
-    rec.lang = "es-AR";
-    rec.continuous = true;
-    rec.interim = true;
-    rec.addEventListener('result' , iniciar );
-
+if (!("webkitSpeechRecognition" in window)) {
+  alert("sin reconocimiento de voz");
+} else {
+  rec = new webkitSpeechRecognition();
+  rec.lang = "en-US";
+  rec.continuous = true;
+  rec.interim = true;
+  rec.addEventListener("result", iniciar);
 }
 
 function iniciar(event) {
-    for (let i = event.resultIndex ; i < event.results.length; i++) {
-        document.getElementById('texto').innerHTML = event.results[i][0].transcript;
-    }
+  for (let i = event.resultIndex; i < event.results.length; i++) {
+    document.getElementById("texto").innerHTML +=
+      " " + event.results[i][0].transcript;
+  }
 }
 
+function reset() {
+  document.getElementById("texto").innerHTML = "";
+}
 
-rec.start();
+function iniciarReconocimiento() {
+  rec.start();
+}
 
+function detenerReconocimiento() {
+  rec.stop();
+}
